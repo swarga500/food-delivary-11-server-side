0d5ca575e24150e7e343cccfa50118dceeab5457
swarga500 <swarga500@gmail.com>
2021-10-30 01:54:36 +0600

    service add & load

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+
+// middlewre 
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rxher.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run (){
+    try{
+        client.connect();
+        const database =client.db('airtickets');
+        const ticketsCollection = database.collection('tickets')
+        
+        // get api 
+        app.get('/tickets', async (req,res)=>{
+              const cursor = ticketsCollection.find({});
+              const tickets = await cursor.toArray();
+              res.send(tickets)
+        })
+
+        // post api
+        app.post('/tickets', async (req,res) =>{
+            const ticket = req.body;
+            console.log('hit')
+            const result = await ticketsCollection.insertOne(ticket)
+            console.log(result)
+            res.json(result)
+        })
+
+
+    }
+    finally{
+        // await client.close()
+    }
+
+}
+
+run().catch(console.dir)
+app.get('/', (req,res) =>{
+    res.send("servoer running")
+});
+
+
+app.listen(port, ()=>{
+    console.log('server is runnig', port)
+})
